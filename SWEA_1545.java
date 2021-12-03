7742f1a5867b872653afe49caccf730a0251bfd2
gkdud583 <qqaa5533@naver.com>
2021-12-03 09:51:24 +0900

    SWEA 1545
    SWEA 1545 D1

---
+++ SWEA_1545.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.io.*;
+
+
+
+public class SWEA_1545 {
+    public static void main(String[]args) throws Exception{
+
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+
+        for(int i=N; i>=0; i--){
+            System.out.print(i+" ");
+        }
+    }
+}
