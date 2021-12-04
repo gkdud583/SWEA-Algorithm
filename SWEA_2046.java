de85dd75a58f0e4f5bc356a9d8b5e70a63670922
gkdud583 <qqaa5533@naver.com>
2021-12-04 14:08:27 +0900

    SWEA 2046
    SWEA 2046 D1

---
+++ SWEA_2046.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.io.*;
+
+
+
+public class SWEA_2046 {
+    public static void main(String[]args) throws Exception{
+
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+
+        for(int i=1; i<=N; i++){
+            System.out.print("#");
+        }
+    }
+}
