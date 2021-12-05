219551949282fc3d825277c7e2ca04c37cbeb0a2
gkdud583 <qqaa5533@naver.com>
2021-12-05 11:30:05 +0900

    SWEA 2058
    SWEA 2058 D1

---
+++ SWEA_2058.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+import java.io.*;
+
+
+
+public class SWEA_2058 {
+    public static void main(String[]args) throws Exception{
+
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+
+        int sum = 0;
+        while(N > 0){
+            sum += N % 10;
+            N /= 10;
+        }
+        System.out.println(sum);
+    }
+}
