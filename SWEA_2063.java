6003f40fb74b4791d4401d04803f97cf096d5de0
gkdud583 <qqaa5533@naver.com>
2021-12-05 11:40:18 +0900

    SWEA 2063
    SWEA 2063 D1

---
+++ SWEA_2063.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+import java.io.*;
+
+
+
+public class SWEA_2063 {
+    public static void main(String[]args) throws Exception{
+
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+
+    
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=1; i<=N; i++){
+            int num = sc.nextInt();
+            list.add(num);
+        }
+
+        Collections.sort(list);
+        System.out.println(list.get(list.size()/2));
+    }
+}
