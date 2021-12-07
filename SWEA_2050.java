16e93dd5987b13ca25507a4e13640dfd828169b5
gkdud583 <qqaa5533@naver.com>
2021-12-07 10:41:40 +0900

    SWEA 2050
    SWEA 2050 D1

---
+++ SWEA_2050.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+import java.io.*;
+
+
+
+public class SWEA_2050 {
+    public static void main(String[]args) throws Exception{
+
+        Scanner sc = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
+
+        String input = sc.nextLine();
+
+        for(int i=0; i<input.length(); i++){
+            char c = input.charAt(i);
+
+            if(c >= 97 && c <= 122){
+                c = (char)(c - 32);
+            }
+
+            sb.append(c - 64).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
