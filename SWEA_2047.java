b733b464c3c06b4b6a4960e06eb966f77c46b5cf
gkdud583 <qqaa5533@naver.com>
2021-12-07 10:23:33 +0900

    SWEA 2047
    SWEA 2047 D1

---
+++ SWEA_2047.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+import java.io.*;
+
+
+
+public class SWEA_2047 {
+    public static void main(String[]args) throws Exception{
+
+        Scanner sc = new Scanner(System.in);
+        
+        StringBuffer ans = new StringBuffer();
+        String input = sc.nextLine();
+
+        for(int i=0; i<input.length(); i++){
+            char c = input.charAt(i);
+
+            if(c >= 97 && c <= 122){
+                ans.append((char)(c - 32));
+            }else{
+                ans.append(c);
+            }
+        }
+        System.out.println(ans);
+    }
+}
